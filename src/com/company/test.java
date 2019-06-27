7a4c8831789787d3356dbc170e0eecebe40b02b9
liumeilin <843895894@qq.com>
2019-06-27 15:34:06 +0800

    test1 modify 2

--- src/com/company/test.java
+++ src/com/company/test.java
@@ -3,6 +3,7 @@ package com.company;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World 1");
         System.out.println("Hello World 2");
     }
 }
